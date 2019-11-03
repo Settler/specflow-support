39038cf2c22070a63a2c8f09f7b9df481234e4b2
Settler <104102+Settler@users.noreply.github.com>
2019-11-03 23:18:16 +0300

    Added table cell content to ToString

--- src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/Psi/GherkinTableCell.cs
+++ src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/Psi/GherkinTableCell.cs
@@ -5,5 +5,11 @@ namespace ReSharperPlugin.SpecflowRiderPlugin.Psi
         public GherkinTableCell() : base(GherkinNodeTypes.TABLE_CELL)
         {
         }
+        
+        public override string ToString()
+        {
+            var textToken = this.FindChild<GherkinToken>(o => o.NodeType == GherkinTokenTypes.TABLE_CELL);
+            return $"GherkinTableCell: {textToken?.GetText()}";
+        }
     }
 }
